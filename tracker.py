ac256f6aa852a3624df16866ec872c182ba9a6fd
Hangfeng-Li <113675751+Hangfeng-Li@users.noreply.github.com>
2022-09-30 13:11:16 +0800

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Sep 26 13:38:02 2022
 
-@author: e0947330
+@author: Li Hangfeng
 """
 
 import math
@@ -10,29 +10,29 @@ import math
 
 class EuclideanDistTracker:
     def __init__(self):
-        # 存储对象的中心位置
+        # The position that store the central point location
         self.center_points = {}
-        # 保持id的计数
-        # 每次检测到新的对象 id 时，计数将增加 1
+        
+        # The count is incremented by one each time a new object ID is detected
         self.id_count = 0
 
 
     def update(self, objects_rect):
-        # 对象框和 ID
+        # Object box and ID
         objects_bbs_ids = []
 
-        # 获取新对象的中心点
+        # Gets the center point of the new object
         for rect in objects_rect:
             x, y = rect
             cx = x
             cy = y
 
-            # 查明是否已经检测到该对象
+            # Find out if the object has been detected
             same_object_detected = False
             for id, pt in self.center_points.items():
-            	# 计算中心点之间的欧式距离
+            	# Compute the Euclidean distance between the center points
                 dist = math.hypot(cx - pt[0], cy - pt[1])
-				# 如果欧氏距离小于25即表明是相同的目标
+				# If the Euclidean distance is less than 10, it indicates the same target
                 if dist < 10:
                     self.center_points[id] = (cx, cy)
                     print(self.center_points)
@@ -40,19 +40,19 @@ class EuclideanDistTracker:
                     same_object_detected = True
                     break
 
-            # 检测到新对象，我们将 ID 分配给该对象
+            # A new object is detected and we assign an ID to the object
             if same_object_detected is False:
                 self.center_points[self.id_count] = (cx, cy)
                 objects_bbs_ids.append([x, y, self.id_count])
                 self.id_count += 1
 
-        # 按中心点清理字典以删除不再使用的 IDS
+        # The dictionary is cleaned by the center point to remove IDS that are no longer used
         new_center_points = {}
         for obj_bb_id in objects_bbs_ids:
             _, _,object_id = obj_bb_id
             center = self.center_points[object_id]
             new_center_points[object_id] = center
 
-        # 更新字典
+        # Update the dictionary
         self.center_points = new_center_points.copy()
         return objects_bbs_ids
