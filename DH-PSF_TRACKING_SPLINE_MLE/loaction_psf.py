f41afa41a7f304f37ab46e48175207692d0f1335
Hangfeng-Li <113675751+Hangfeng-Li@users.noreply.github.com>
2022-10-07 17:59:40 +0800

    Update loaction_psf.py

--- DH-PSF_TRACKING_SPLINE_MLE/loaction_psf.py
+++ DH-PSF_TRACKING_SPLINE_MLE/loaction_psf.py
@@ -12,7 +12,8 @@ import matplotlib.pyplot as plt
 input0 = np.array(tifffile.imread('G:/tony lab/cx/E 488 off 561 on 001-half.tif'))
 input0_size = input0.shape
 
-scharr=np.ones((40,40)) 
+
+scharr=np.ones((30,30)) 
 
 
 
@@ -20,3 +21,11 @@ scharr=np.ones((40,40))
 grad=signal.convolve2d(input0[10,:,:],scharr,boundary='symm',mode='same') 
 plt.imshow(input0[10,:,:], origin='lower')
 plt.imshow(grad, origin='lower')
+GRAD=((grad>7000) & (grad<10000))
+plt.imshow(GRAD, origin='lower')
+
+text11=(np.multiply(input0[10,:,:],GRAD))
+text12=np.array(text11,dtype=int)
+
+from skimage import io
+io.imsave('G:/tony lab/cx//change.tif', text12)
